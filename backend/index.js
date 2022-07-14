63ece8f7a19bc6b3b6dd2a9b8921196f8a6cbae2
RaidasGrisk <mrraidas@gmail.com>
2022-07-14 14:02:11 +0300

    add mongo

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,42 @@
-const express = require("express");
-const app = express();
+const MongoClient = require('mongodb').MongoClient
+require('dotenv').config()
 
-app.use(express.json({ extended: false }));
+const express = require("express")
+const app = express()
+
+app.use(express.json({ extended: false }))
+
+// mongodb credentials
+const mongo_acc = process.env.mongo_acc
+const mongo_psw = process.env.mongo_psw
+const connectionString = `mongodb+srv://${mongo_acc}:${mongo_psw}@cluster0.ejotl.gcp.mongodb.net/test?retryWrites=true&w=majority`
+console.log(mongo_acc, mongo_psw)
 
 app.get("/", async (req, res) => {
   try {
     res.json({
       status: 200,
       message: "Ok",
-    });
+    })
   } catch (error) {
     console.error(error);
-    return res.status(500).send("Error");
+    return res.status(500).send("Error")
   }
-});
+})
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
+MongoClient.connect(connectionString, (err, client) => {
+  if (err) return console.error(err)
+  console.log('Connected to Database')
+
+  const db = client.db('fundingo')
+
+  app.get('/test', function (req, res) {
+    db.collection('subs').find().toArray().then(resp => {
+      res.send(resp)
+    })
+  })
+})
+
+
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`))
